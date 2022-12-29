7303f55a55fe8d2692b6dadf8aa5e9cd43c3dc5d
BrianOuOb <113296783+BrianOuOb@users.noreply.github.com>
2022-12-29 20:19:58 +0800

    Create Class09.java

---
+++ Class09.java
@@ -0,0 +1,14 @@
+package pack9;  
+import pack9.sub1.CSphere;
+import pack9.sub2.CTrapezoid;   
+
+public class Main
+{
+   public static void main(String args[])  
+   {
+      CSphere sp=new CSphere(2);
+      CTrapezoid tra=new CTrapezoid(2,3,4);
+      sp.show();
+      tra.show();
+   }
+}
